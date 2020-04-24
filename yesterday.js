2f5a01feae5549b66328788da692c28e1dae3009
ewami <efili1989@gmail.com>
2020-04-24 22:44:11 +0200

    last changes in get.app

--- yesterday.js
+++ yesterday.js
@@ -2,6 +2,19 @@ const axios = require("axios");
 const express = require("express");
 
 const app = express();
+
+const sourceApi = "http://localhost:30000/days";
+
+app.get("/days", async (req, res) => {
+  try {
+    const sourceRes = await axios.get(sourceApi);
+    const { data } = sourceRes;
+    res.send(data);
+  } catch (error) {
+    console.log("error.message test:", error.message);
+  }
+});
+
 const port = 40000;
 
 app.listen(port, console.log(`Server started on: ${port}`));
